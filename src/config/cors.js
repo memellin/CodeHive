93c52ed268cb68090db0a2bbac3a00797118aa3f
Enock <enocksm@outlook.com>
2025-06-02 11:44:04 -0300

    adicionando endereco do front-end no cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 // Configurações do CORS
 const corsOptions = {
-  origin: ['http://localhost:3333', 'http://localhost:3001', 'http://localhost:3000'], // Permitir apenas essas origens
+  origin: ['http://localhost:3333', 'http://localhost:3001', 'http://localhost:3000', 'https://code-hive-front-end-ike7.vercel.app/', 'https://code-hive-front-end-ike7-memellins-projects.vercel.app/', 'https://code-hive-front-end-ike7-git-main-memellins-projects.vercel.app/'], // Permitir apenas essas origens
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
   credentials: true // Permitir cookies e autenticação (opcional)
